71693ec0bdf2507e23c7693abdd6a2aa53653b0c
daniel107x <DMejiaV310796@outlook.com>
2023-01-09 08:14:34 -0600

    Added alternative

--- src/com/problems/dp/HouseRobber2.java
+++ src/com/problems/dp/HouseRobber2.java
@@ -2,7 +2,7 @@ package com.problems.dp;
 
 public class HouseRobber2 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
+        int[] nums = {1,1,3,6,7,10,7,1,8,5,9,1,4,4,3};
         System.out.println(rob(nums));
     }
 
@@ -14,13 +14,17 @@ public class HouseRobber2 {
             int pos;
             if(next == latest[0][1]){
                 //TODO: Add condition to check what happens if we remove the first element;
-                if(nums[i] > latest[1][0]){
-                    val = nums[i];
-                    pos = i;
-                }else{
-                    val = latest[1][0];
-                    pos = latest[1][1];
-                }
+                int first = nums[next];
+                int alt = latest[0][0] + nums[i] - first;
+//                if(nums[i] > latest[1][0]){
+//                    val = nums[i];
+//                    pos = i;
+//                }else{
+//                    val = latest[1][0];
+//                    pos = latest[1][1];
+//                }
+                val = Math.max(alt, Math.max(latest[1][0], nums[i]));
+                pos = 0;
             }else{
                 if(nums[i] + latest[0][0] > latest[1][0]){
                     val = nums[i] + latest[0][0];
