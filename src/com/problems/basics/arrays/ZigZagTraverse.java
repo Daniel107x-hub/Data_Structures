a63e37c6d774316a189c8d2cc6d537e944bff4aa
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-04-16 18:38:41 -0600

    Added solution to zigzag traverse

--- src/com/problems/basics/arrays/ZigZagTraverse.java
+++ src/com/problems/basics/arrays/ZigZagTraverse.java
@@ -1,12 +1,28 @@
 package com.problems.basics.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ZigZagTraverse {
     public static void main(String[] args) {
-
+        int[][] array = {
+                {1, 3, 4, 10},
+                {2, 5, 9, 11},
+                {6, 8, 12, 15},
+                {7, 13, 14, 16}
+        };
+        List<List<Integer>> arrayList = new ArrayList<>();
+        for(int[] row: array){
+            List<Integer> rowList = new ArrayList<>();
+            for(int e : row){
+                rowList.add(e);
+            }
+            arrayList.add(rowList);
+        }
+        List<Integer> traverse = zigzagTraverse(arrayList);
+        for(int e : traverse) System.out.print(e + " ");
     }
 
     public enum Direction{
@@ -24,7 +40,9 @@ public class ZigZagTraverse {
             int i = 0, j = 0;
             results.add(array.get(i).get(j));
             Direction direction = Direction.DOWN;
+            i++;
             while(i < array.size() && j < array.get(0).size()){
+                results.add(array.get(i).get(j));
                 direction = getNextDirection(i, j, direction, array);
                 if(direction.equals(Direction.DOWN)) i++;
                 else if(direction.equals(Direction.DIAG_UP)){
@@ -48,7 +66,7 @@ public class ZigZagTraverse {
                 break;
 
             case DIAG_UP:
-                if(row > 0 && col < array.get(0).size()) break;
+                if(row > 0 && col < array.get(0).size() - 1) break;
                 else if(col == array.get(0).size() - 1) direction = Direction.DOWN;
                 else direction = Direction.RIGHT;
                 break;
@@ -59,7 +77,7 @@ public class ZigZagTraverse {
                 break;
 
             case DIAG_DOWN:
-                if(row < array.size() && col > 0) break;
+                if(row < array.size() - 1 && col > 0) break;
                 else if(row == array.size() - 1) direction = Direction.RIGHT;
                 else direction = Direction.DOWN;
                 break;
